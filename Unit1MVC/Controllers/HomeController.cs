ce44aa182459d981321a00f19801e7ae32a78be4
ERedEarth99 <ezra.redearth@southeasttech.edu>
2024-09-23 12:38:11 -0500

    Add project files.

---
+++ Unit1MVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Unit1MVC.Models;
+
+namespace Unit1MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var displays = new List<Display>
+            {
+            new Display { Title = "CE", ImageFileName = "A1.png" },
+            new Display { Title = "TA", ImageFileName = "A2.png" },
+            new Display { Title = "SF", ImageFileName = "P1.png" },
+            new Display { Title = "CT", ImageFileName = "P2.png" }
+        };
+            return View(displays);
+        }
+    }
+}
---
+++ Unit1MVC/Models/Display.cs
@@ -0,0 +1,8 @@
+﻿namespace Unit1MVC.Models
+{
+    public class Display
+    {
+        public string Title { get; set; }
+        public string ImageFileName { get; set; }
+    }
+}
